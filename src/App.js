db7a69af8e963253df6befe9c1097667c92f6d9e
LabibIslam <labib.islam.practice@gmail.com>
2022-05-16 17:00:56 +0600

    completed 71.3 video

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Navbar from './pages/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Login from './pages/Login/Login';
+import Appointment from './pages/Appointment/Appointment';
+import Review from './pages/Review/Review';
+import Contactus from './pages/Contact/Contactus';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path='login' element={<Login />}></Route>
+        <Route path='appointment' element={<Appointment />}></Route>
+        <Route path='review' element={<Review />}></Route>
+        <Route path='contact' element={<Contactus />}></Route>
       </Routes>
     </div>
   );
