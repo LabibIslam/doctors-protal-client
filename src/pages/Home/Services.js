e466c5f0b5c9c4f6023cd85883ec4480fa44e694
LabibIslam <labib.islam.practice@gmail.com>
2022-05-18 16:29:40 +0600

    completed 71.6 video

---
+++ src/pages/Home/Services.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import fluoride from '../../assets/images/fluoride.png';
+import cavity from '../../assets/images/cavity.png';
+import whitening from '../../assets/images/whitening.png';
+import Service from './Service';
+import Treatment from '../../assets/images/treatment.png';
+
+const Services = () => {
+    const services = [
+        {
+            _id: 1,
+            name: "Fluoride Treatment",
+            description: "",
+            img: fluoride
+        },
+        {
+            _id: 2,
+            name: "Cavity Filling",
+            description: "",
+            img: cavity
+        },
+        {
+            _id: 3,
+            name: "Teeth Whitening",
+            description: "",
+            img: whitening
+        },
+
+    ];
+    return (
+        <div className='my-28'>
+            <div className='text-center'>
+                <h3 className='text-primary text-lg font-bold uppercase'>Our services</h3>
+                <h2 className='text-4xl'>Services We Provide</h2>
+            </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 pt-14          '>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
+            <div>
+                <div class="hero min-h-screen">
+                    <div class="hero-content flex-col lg:flex-row gap-20">
+                        <img src={Treatment} class="max-w-sm rounded-lg" />
+                        <div className=''>
+                            <h1 class="text-5xl font-bold">Exceptional Dental </h1>
+                            <h1 class="text-5xl font-bold">Care, on Your Terms</h1>
+                            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                            <button class="btn btn-primary text-white uppercase font-bold bg-gradient-to-r from-secondary to-primary">Get Started</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
